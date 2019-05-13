1ecfd1b3de655721bcf6a7a70c16fd6b80201c05
Šimon Slamka <simon.slamka@adlerka.sk>
2019-05-13 20:39:13 +0200

    fixed caffeine value output to screen

--- app/src/main/java/com/simtoonsoftware/caffeinator/MainActivity.java
+++ app/src/main/java/com/simtoonsoftware/caffeinator/MainActivity.java
@@ -1,53 +1,37 @@
 package com.simtoonsoftware.caffeinator;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    float var;
+    //variables
+    float caffeineIntakeValue;
 
-    EditText input_CaffeineIntakeValue;// = findViewById(R.id.input_CaffeineIntakeValue);
-    TextView textView;
+    EditText input_CaffeineIntakeValue;
+    TextView text_caffeineIntakeValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.textView);
+        text_caffeineIntakeValue = (TextView) findViewById(R.id.text_caffeineIntakeValue);
         Button btn_addCaffeineIntake = findViewById(R.id.btn_addCaffeineIntake);
-       input_CaffeineIntakeValue = (EditText) findViewById(R.id.input_CaffeineIntakeValue);
+        input_CaffeineIntakeValue = (EditText) findViewById(R.id.input_CaffeineIntakeValue);
 
 
-        input_CaffeineIntakeValue.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
-
-                boolean handled = false;
-                if (i == EditorInfo.IME_ACTION_GO) {
-                    Toast.makeText(MainActivity.this, "Current caffeine level: "
-                            + getApplicationContext().getResources().getInteger(R.integer.caffeineIntakeValue), Toast.LENGTH_SHORT).show();
-                }
-                return handled;
-            }
-
-        });
         btn_addCaffeineIntake.setOnClickListener(new View.OnClickListener() {
             @Override
         public void onClick(View v) {
            // startActivity(new Intent(MainActivity.this, AddCaffeineIntakeActivity.class));
-                var = Float.parseFloat(input_CaffeineIntakeValue.getText() + "");
-                textView.setText("Coffeine level: " + var);
-            System.out.println("Switched to the 'AddCaffeineIntakeActivity' Activity" + var); //debug text, shows only in logcat
+                caffeineIntakeValue = caffeineIntakeValue + Float.parseFloat(input_CaffeineIntakeValue.getText() + "");
+                text_caffeineIntakeValue.setText("" + caffeineIntakeValue);
+            System.out.println("Switched to the 'AddCaffeineIntakeActivity' Activity" + caffeineIntakeValue); //debug text, shows only in logcat
         }});
     }
 }
