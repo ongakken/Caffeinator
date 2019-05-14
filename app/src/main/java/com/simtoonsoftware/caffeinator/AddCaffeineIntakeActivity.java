905cce5ccecf6546c95d86ffea94aa259edaaf49
Simtoon Software <simon.slamka@adlerka.sk>
2019-05-14 08:05:05 +0200

    moved the caffeine intake input to AddCaffeineIntakeActivity

--- app/src/main/java/com/simtoonsoftware/caffeinator/AddCaffeineIntakeActivity.java
+++ app/src/main/java/com/simtoonsoftware/caffeinator/AddCaffeineIntakeActivity.java
@@ -1,6 +1,23 @@
 package com.simtoonsoftware.caffeinator;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AddCaffeineIntakeActivity extends AppCompatActivity {
+
+    //declarations and definitions
+    text_caffeineIntakeValue = (TextView) findViewById(R.id.text_caffeineIntakeValue);
+    Button btn_addCaffeineIntake = findViewById(R.id.btn_addCaffeineIntake);
+    input_CaffeineIntakeValue = (EditText) findViewById(R.id.input_CaffeineIntakeValue);
+    float caffeineIntakeValue;
+
+    btn_addCaffeineIntake.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // startActivity(new Intent(MainActivity.this, AddCaffeineIntakeActivity.class));
+            caffeineIntakeValue = caffeineIntakeValue + Float.parseFloat(input_CaffeineIntakeValue.getText() + "");
+            text_caffeineIntakeValue.setText("" + caffeineIntakeValue);
 }
--- app/src/main/java/com/simtoonsoftware/caffeinator/MainActivity.java
+++ app/src/main/java/com/simtoonsoftware/caffeinator/MainActivity.java
@@ -9,8 +9,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    //variables
+    //definitions and declarations
     float caffeineIntakeValue;
+    text_caffeineIntakeValue = (TextView) findViewById(R.id.text_caffeineIntakeValue);
 
     EditText input_CaffeineIntakeValue;
     TextView text_caffeineIntakeValue;
@@ -19,18 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        text_caffeineIntakeValue = (TextView) findViewById(R.id.text_caffeineIntakeValue);
-        Button btn_addCaffeineIntake = findViewById(R.id.btn_addCaffeineIntake);
-        input_CaffeineIntakeValue = (EditText) findViewById(R.id.input_CaffeineIntakeValue);
-
-
-        btn_addCaffeineIntake.setOnClickListener(new View.OnClickListener() {
-            @Override
-        public void onClick(View v) {
-           // startActivity(new Intent(MainActivity.this, AddCaffeineIntakeActivity.class));
-                caffeineIntakeValue = caffeineIntakeValue + Float.parseFloat(input_CaffeineIntakeValue.getText() + "");
-                text_caffeineIntakeValue.setText("" + caffeineIntakeValue);
+        text_caffeineIntakeValue.setText("" + caffeineIntakeValue);
             //System.out.println("Switched to the 'AddCaffeineIntakeActivity' Activity" + caffeineIntakeValue); //debug text, shows only in logcat
         }});
     }
