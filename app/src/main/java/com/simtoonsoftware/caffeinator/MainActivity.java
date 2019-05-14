bd6e6b3f9641f7b840160fe4e71b02e5283431e5
Šimon Slamka <simon.slamka@adlerka.sk>
2019-05-14 19:06:10 +0200

    updateBug

--- app/src/main/java/com/simtoonsoftware/caffeinator/MainActivity.java
+++ app/src/main/java/com/simtoonsoftware/caffeinator/MainActivity.java
@@ -1,22 +1,35 @@
 package com.simtoonsoftware.caffeinator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     //definitions and declarations
-    float caffeineIntakeValue;
+    float caffeineIntakeValue = 0;
     TextView text_caffeineIntakeValue;
+    Button btn_addCaffeineIntake = findViewById(R.id.btn_addCaffeineIntake);
+
+    //icon - cup of coffee HEX #FFA500
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        text_caffeineIntakeValue = findViewById(R.id.text_caffeineIntakeValue);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text_caffeineIntakeValue = findViewById(R.id.text_caffeineIntakeValue);
         text_caffeineIntakeValue.setText("" + caffeineIntakeValue);
-        //System.out.println("Switched to the 'AddCaffeineIntakeActivity' Activity" + caffeineIntakeValue); //debug text, shows only in logcat
+        btn_addCaffeineIntake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent AddCaffeineIntakeActivityIntent = new Intent(getApplicationContext(), AddCaffeineIntakeActivity.class);
+                startActivity(AddCaffeineIntakeActivityIntent);
+            }
+        });
+
+
     }}
