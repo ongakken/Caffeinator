2467e94a90b8f87e102460a894ad436545c33b53
simonSlamkaSMD <SMDexec000@protonmail.ch>
2019-06-30 21:23:15 +0200

    added the Firebase API and migrated the project to AndroidX;

--- app/src/main/java/sk/smdtech/caffeinator/Activities/GraphActivity.java
+++ app/src/main/java/sk/smdtech/caffeinator/Activities/GraphActivity.java
@@ -3,11 +3,11 @@
 package sk.smdtech.caffeinator.Activities;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import com.google.android.material.navigation.NavigationView;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
