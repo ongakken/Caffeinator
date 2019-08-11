ae61a8f4af10cb48232ba08bebd7f3e89391e870
EltnoSetan <dominik.dielessen@azet.sk>
2019-08-11 20:01:54 +0200

    fixed the scrollbar in privacy policy

--- app/src/main/java/sk/smdtech/caffeinator/Activities/PrivacyPolicyActivity.java
+++ app/src/main/java/sk/smdtech/caffeinator/Activities/PrivacyPolicyActivity.java
@@ -7,14 +7,19 @@ package sk.smdtech.caffeinator.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 import sk.smdtech.caffeinator.R;
 
 public class PrivacyPolicyActivity extends AppCompatActivity {
+    TextView privacyPolicy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy_policy);
+
+        privacyPolicy = findViewById(R.id.textView3);
+        privacyPolicy.setMovementMethod(new ScrollingMovementMethod());
     }
 }
